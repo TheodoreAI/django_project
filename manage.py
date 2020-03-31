a68f14c304d09d1594ec6d5d22a2bdf4dead95ed
Mateo Estrada <mateoej12@gmail.com>
2020-03-31 14:44:48 -0700

    I removed the pyobjc

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import django
 
 
 def main():
@@ -18,4 +19,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main() 
+    main()
