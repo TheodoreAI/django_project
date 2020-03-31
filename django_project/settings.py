1118be78ed6942976fcade23602684c501d2811d
Mateo Estrada <mateoej12@gmail.com>
2020-03-31 16:39:25 -0700

    Updated the settings file

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku 
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -158,3 +160,4 @@ EMAIL_HOST_PASSWORD = os.environ.get('db_pass')
 db_user = os.environ.get('db_user')
 db_password = os.environ.get('db_pass')
 
+django_heroku.settings(locals())
