b0f8fe608e061f01a23c9f76d21bd909a11fdc29
Mateo Estrada <mateoej12@gmail.com>
2020-04-12 14:28:55 -0700

    Debug set to secure measure.

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,11 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-# (os.environ.get('DEBUG_VALUE') == 'True')
-
-
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'TRUE')
 
 
 ALLOWED_HOSTS = ['bitbrane.herokuapp.com']
