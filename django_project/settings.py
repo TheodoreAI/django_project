e37b6c1dfa46ec9673ef984c5fae0a925de761cc
Mateo Estrada <mateoej12@gmail.com>
2020-06-26 17:08:57 -0700

    Settings.py.

--- django_project/settings.py
+++ django_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'kjahsfpui[3488978798709879%%728376(**'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'TRUE')
+DEBUG = True
 
 
 ALLOWED_HOSTS = []
